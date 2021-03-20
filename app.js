fc8ef1e4fa0cfd32aeda6538201833417d4a847c
Sanjeev Singla <f20170152@pilani.bits-pilani.ac.in>
2021-03-20 09:56:07 +0530

    Added a non-efficeint amthod for implmenting dynamic relations which can lead to infinte loops

--- app.js
+++ app.js
@@ -24,12 +24,14 @@ app.use(
             description: String!
             price: Float!
             date: String!
+            creator: User!
         }
 
         type User {
           _id: ID
           email: String!
           password: String
+          createdEvents: [Event!]
         }
 
         input EventInput {
@@ -62,9 +64,17 @@ app.use(
       events: () => {
         // return EVENTS_DATABASE;
         return Event.find()
+          .populate("creator")
           .then((events) => {
             return events.map((event) => {
-              return { ...event._doc, _id: event._doc._id.toString() };
+              return {
+                ...event._doc,
+                _id: event._doc._id.toString(),
+                creator: {
+                  ...event._doc.creator._doc,
+                  _id: event._doc.creator.id,
+                },
+              };
             });
           })
           .catch((err) => {
